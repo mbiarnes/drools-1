5a832b03214527efdebd97ab03cde674b659c2c5
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2018-12-11 13:31:53 +0100

    using variables

--- drools-wb/base/build.sh
+++ drools-wb/base/build.sh
@@ -4,11 +4,24 @@
 # Drools Workbench - Docker image build script
 # ********************************************
 
+
+PROPERTY_FILE="../../image.properties"
+
+function getProperty {
+   IMAGE_PROP=$1
+   KIE_PROP=`cat $PROPERTY_FILE | grep "$IMAGE_PROP" | cut -d'=' -f2`
+   echo $KIE_PROP
+}
+
+KIE_VERSION=$(getProperty)
+
+echo "KIE_VERSION =" $KIE_VERSION
+
 IMAGE_NAME="jboss/drools-workbench"
-IMAGE_TAG="latest"
+IMAGE_TAG="7.15.0.Final"
 
 
 # Build the container image.
 echo "Building the Docker container for $IMAGE_NAME:$IMAGE_TAG.."
-docker build --rm -t $IMAGE_NAME:$IMAGE_TAG .
+docker build --rm --build-arg KIE_VERSION=$KIE_VERSION -t $IMAGE_NAME:$IMAGE_TAG .
 echo "Build done"
